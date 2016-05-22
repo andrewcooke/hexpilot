d1c6ca9e1fdf7fe466a6723886cb0a4d93a99cb1
andrew cooke <andrew@acooke.org>
2016-05-22 15:10:28 -0400

    change to glfw

--- src/main.c
+++ src/main.c
@@ -1,24 +1,34 @@
-#include <GL/glut.h>
+
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
 
 void display() {
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-  glClear(GL_COLOR_BUFFER_BIT);
-  glBegin(GL_QUADS);
-  glColor3f(1.0f, 0.0f, 0.0f);
-  glVertex2f(-0.5f, -0.5f);
-  glVertex2f( 0.5f, -0.5f);
-  glVertex2f( 0.5f,  0.5f);
-  glVertex2f(-0.5f,  0.5f);
-  glEnd();
-  glFlush();
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f( 0.5f, -0.5f);
+    glVertex2f( 0.5f,  0.5f);
+    glVertex2f(-0.5f,  0.5f);
+    glEnd();
+    glFlush();
 }
  
 int main(int argc, char** argv) {
-  glutInit(&argc, argv);
-  glutCreateWindow("OpenGL Setup Test");
-  glutInitWindowSize(320, 320);
-  glutInitWindowPosition(50, 50);
-  glutDisplayFunc(display);
-  glutMainLoop();
-  return 0;
+    GLFWwindow *window = NULL;
+    if (!glfwInit()) return 1;
+    window = glfwCreateWindow(320, 320, "OpenGL Setup Test", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window)) {
+        display();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
 }
