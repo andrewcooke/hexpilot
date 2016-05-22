383b9c9213839c1ce13a4a26de7f7a5ecdfcbc0d
andrew cooke <andrew@acooke.org>
2016-05-22 16:30:28 -0400

    add dep on liblu

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 
 #include <GLFW/glfw3.h>
+#include "lu/log.h"
 
 void display() {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -13,8 +14,15 @@ void display() {
     glEnd();
     glFlush();
 }
+
+static lulog *LOG = NULL;
+
+void on_error(int error, char *message) {
+    luerror(LOG, "%d: %s", error, message);
+}
  
 int main(int argc, char** argv) {
+    lulog_mkstderr(&LOG, lulog_level_debug);
     GLFWwindow *window = NULL;
     if (!glfwInit()) return 1;
     window = glfwCreateWindow(320, 320, "OpenGL Setup Test", NULL, NULL);
