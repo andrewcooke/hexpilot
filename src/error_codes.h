fa9694606f707b64f623ac8ac065bea0949d6734
andrew cooke <andrew@acooke.org>
2016-05-22 19:43:27 -0400

    error checking

---
+++ src/error_codes.h
@@ -0,0 +1,8 @@
+
+#ifndef HP_ERROR_CODES_H
+#define HP_ERROR_CODES_H
+
+#define HP_OFFSET 0x100
+#define HP_ERR_GLFW (HP_OFFSET | 1)
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 
 #include <GLFW/glfw3.h>
-#include "lu/log.h"
 
-void display() {
+#include "error_codes.h"
+#include "lu/log.h"
+#include "lu/status.h"
+
+
+static int display(lulog *log) {
+    LU_STATUS
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_QUADS);
@@ -13,29 +18,43 @@ void display() {
     glVertex2f(-0.5f,  0.5f);
     glEnd();
     glFlush();
+    LU_NO_CLEANUP
 }
 
-static lulog *LOG = NULL;
+int main_loop(lulog *log) {
 
-void on_error(int error, char *message) {
-    luerror(LOG, "%d: %s", error, message);
-}
- 
-int main(int argc, char** argv) {
-    lulog_mkstderr(&LOG, lulog_level_debug);
+    LU_STATUS
     GLFWwindow *window = NULL;
-    if (!glfwInit()) return 1;
-    window = glfwCreateWindow(320, 320, "OpenGL Setup Test", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        return 1;
-    }
+
+    LU_ASSERT(glfwInit(),
+            HP_ERR_GLFW, log, "Could not start GLFW")
+    LU_ASSERT(window = glfwCreateWindow(320, 320, "hexpilot", NULL, NULL),
+            HP_ERR_GLFW, log, "Could not create window")
+
     glfwMakeContextCurrent(window);
     while (!glfwWindowShouldClose(window)) {
-        display();
+        LU_CHECK(display(log))
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+LU_CLEANUP
     glfwTerminate();
-    return 0;
+    LU_RETURN
+}
+
+
+static lulog *LOG = NULL;
+
+void on_error(int error, const char *message) {
+    luerror(LOG, "%d: %s", error, message);
+}
+
+int main(int argc, char** argv) {
+    LU_STATUS
+    lulog_mkstderr(&LOG, lulog_level_debug);
+    glfwSetErrorCallback(on_error);
+    LU_CHECK(main_loop(LOG))
+LU_CLEANUP
+    return status ? 1 : 0;
 }
