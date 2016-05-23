f16ac2ba11917bd824913768158fc49ce9ab2b81
andrew cooke <andrew@acooke.org>
2016-05-23 13:22:51 -0400

    adding modern stuff (segfaulting)

--- src/error_codes.h
+++ src/error_codes.h
@@ -4,6 +4,14 @@
 
 #define HP_OFFSET 0x100
 #define HP_ERR_GLFW (HP_OFFSET | 1)
-#define HP_ERR_GL (HP_OFFSET | 2)
+#define HP_ERR_OPENGL (HP_OFFSET | 2)
+
+#define HP_GLCHECK(operation) operation;{\
+    GLenum err = glGetError();\
+    if (err != GL_NO_ERROR) {\
+        luerror(log, "OpenGL Error %x in %s (%s:%d)", err, __func__, __FILE__, __LINE__);\
+        status = HP_ERR_OPENGL;\
+        goto exit;\
+    }}
 
 #endif
--- src/main.c
+++ src/main.c
@@ -22,8 +22,43 @@ static int display(lulog *log) {
     LU_NO_CLEANUP
 }
 
-int createShader(GLenum shaderType) {
-    glCreateShader(shaderType);
+static const char* vertex_shader =
+        "#version 330\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main(){\n"
+        "  gl_Position = position;\n"
+        "}\n";
+
+static const char* fragment_shader =
+        "#version 330\n"
+        "out vec4 outputColor;\n"
+        "void main(){\n"
+        "  outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+        "}\n";
+
+int createShader(lulog *log, GLenum shaderType, const char *source, GLuint *shader) {
+    LU_STATUS
+    HP_GLCHECK(*shader = glCreateShader(shaderType))
+    HP_GLCHECK(glShaderSource(*shader, 1, &source, NULL))
+    HP_GLCHECK(glCompileShader(*shader))
+    GLint compile_status;
+    HP_GLCHECK(glGetShaderiv(*shader, GL_COMPILE_STATUS, &compile_status))
+    if (!compile_status) {
+        GLint log_length;
+        HP_GLCHECK(glGetShaderiv(*shader, GL_INFO_LOG_LENGTH, &log_length))
+        GLchar log_text[log_length];
+        HP_GLCHECK(glGetShaderInfoLog(*shader, log_length, NULL, log_text))
+        lulog_lines(log, lulog_level_debug, log_text);
+    }
+    LU_NO_CLEANUP
+}
+
+int init(lulog *log) {
+    LU_STATUS
+    GLuint shaders[2];
+    LU_CHECK(createShader(log, GL_VERTEX_SHADER, vertex_shader, &shaders[0]))
+    LU_CHECK(createShader(log, GL_FRAGMENT_SHADER, fragment_shader, &shaders[1]))
+    LU_NO_CLEANUP
 }
 
 int main_loop(lulog *log) {
@@ -31,10 +66,16 @@ int main_loop(lulog *log) {
     LU_STATUS
     GLFWwindow *window = NULL;
 
+    // not clear to me to what extent these duplicate or conflict with glad
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     LU_ASSERT(window = glfwCreateWindow(320, 320, "hexpilot", NULL, NULL),
             HP_ERR_GLFW, log, "Could not create window")
 
     glfwMakeContextCurrent(window);
+    LU_CHECK(init(log))
     while (!glfwWindowShouldClose(window)) {
         LU_CHECK(display(log))
         glfwSwapBuffers(window);
