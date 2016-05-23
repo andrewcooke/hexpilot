bc5d03dcc7393071dc2a17383102b28f05fbb207
andrew cooke <andrew@acooke.org>
2016-05-23 16:43:25 -0400

    separate out bookkeeping for opengl

---
+++ src/init.c
@@ -0,0 +1,94 @@
+
+#include "lu/log.h"
+#include "lu/status.h"
+
+#include "init.h"
+#include "error_codes.h"
+
+int create_glfw_context(const lulog *log, GLFWwindow **window) {
+    LU_STATUS
+    // not clear to me to what extent these duplicate or conflict with glad
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    LU_ASSERT(*window = glfwCreateWindow(320, 320, "hexpilot", NULL, NULL),
+            HP_ERR_GLFW, log, "Could not create window")
+    glfwMakeContextCurrent(*window);
+    LU_NO_CLEANUP
+}
+
+int load_opengl_functions(const lulog *log) {
+    LU_STATUS
+    LU_ASSERT(gladLoadGLLoader((GLADloadproc) glfwGetProcAddress),
+            HP_ERR_GLAD, log, "Could not load OpenGL via glad")
+    LU_ASSERT(GLVersion.major > 1, HP_ERR_OPENGL, log,
+            "Bad OpenGL version: %d.%d", GLVersion.major, GLVersion.minor)
+    luinfo(log, "OpenGL %s, GLSL %s",
+            glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
+    LU_NO_CLEANUP
+}
+
+const char *shader_type_str(const lulog *log, GLenum shader_type) {
+    switch (shader_type) {
+    case GL_COMPUTE_SHADER: return "compute";
+    case GL_VERTEX_SHADER: return "vertex";
+    case GL_TESS_CONTROL_SHADER: return "tess control";
+    case GL_TESS_EVALUATION_SHADER: return "tess evaluation";
+    case GL_GEOMETRY_SHADER: return "geometry";
+    case GL_FRAGMENT_SHADER: return "fragment";
+    default:
+        luwarn(log, "Unexpected shader type: %x", shader_type);
+        return "unknown";
+    }
+}
+
+int compile_shader(const lulog *log, GLenum shader_type, const char *source, GLuint *shader) {
+    LU_STATUS
+    ludebug(log, "Compiling %s shader:", shader_type_str(log, shader_type));
+    lulog_lines(log, lulog_level_debug, source);
+    HP_GLCHECK(*shader = glCreateShader(shader_type))
+    HP_GLCHECK(glShaderSource(*shader, 1, &source, NULL))
+    HP_GLCHECK(glCompileShader(*shader))
+    GLint compile_status;
+    HP_GLCHECK(glGetShaderiv(*shader, GL_COMPILE_STATUS, &compile_status))
+    if (!compile_status) {
+        luerror(log, "Failed to compile %s shader", shader_type_str(log, shader_type));
+        GLint log_length;
+        HP_GLCHECK(glGetShaderiv(*shader, GL_INFO_LOG_LENGTH, &log_length))
+        GLchar log_text[log_length];
+        HP_GLCHECK(glGetShaderInfoLog(*shader, log_length, NULL, log_text))
+        lulog_lines(log, lulog_level_debug, log_text);
+        status = HP_ERR_OPENGL;
+        goto exit;
+    }
+    luinfo(log, "Compiled %s shader", shader_type_str(log, shader_type));
+    LU_NO_CLEANUP
+}
+
+int link_program(const lulog *log, const GLuint *shaders, size_t n_shaders) {
+    LU_STATUS
+    HP_GLCHECK(GLuint program = glCreateProgram())
+    for (size_t i = 0; i < n_shaders; ++i) {
+        HP_GLCHECK(glAttachShader(program, shaders[i]))
+    }
+    HP_GLCHECK(glLinkProgram(program))
+    GLint link_status;
+    HP_GLCHECK(glGetProgramiv(program, GL_LINK_STATUS, &link_status))
+    if (!link_status) {
+        luerror(log, "Failed to link program");
+        GLint log_length;
+        HP_GLCHECK(glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_length))
+        GLchar log_text[log_length];
+        HP_GLCHECK(glGetProgramInfoLog(program, log_length, NULL, log_text))
+        lulog_lines(log, lulog_level_debug, log_text);
+        status = HP_ERR_OPENGL;
+        goto exit;
+    }
+    for (size_t i = 0; i < n_shaders; ++i) {
+        HP_GLCHECK(glDetachShader(program, shaders[i]))
+    }
+    luinfo(log, "Linked program with %zu shaders", n_shaders);
+    LU_NO_CLEANUP
+}
+
--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
 
-#include "glad.h"
-#include <GLFW/glfw3.h>
-
-#include "error_codes.h"
 #include "lu/log.h"
 #include "lu/status.h"
+
+#include "init.h"
+#include "error_codes.h"
 
 
 static int display(const lulog *log) {
@@ -36,70 +35,7 @@ static const char* fragment_shader =
         "  outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
         "}\n";
 
-static const char *shader_type_str(const lulog *log, GLenum shader_type) {
-    switch (shader_type) {
-    case GL_COMPUTE_SHADER: return "compute";
-    case GL_VERTEX_SHADER: return "vertex";
-    case GL_TESS_CONTROL_SHADER: return "tess control";
-    case GL_TESS_EVALUATION_SHADER: return "tess evaluation";
-    case GL_GEOMETRY_SHADER: return "geometry";
-    case GL_FRAGMENT_SHADER: return "fragment";
-    default:
-        luwarn(log, "Unexpected shader type: %x", shader_type);
-        return "unknown";
-    }
-}
-
-static int compile_shader(const lulog *log, GLenum shader_type, const char *source, GLuint *shader) {
-    LU_STATUS
-    ludebug(log, "Compiling %s shader:", shader_type_str(log, shader_type));
-    lulog_lines(log, lulog_level_debug, source);
-    HP_GLCHECK(*shader = glCreateShader(shader_type))
-    HP_GLCHECK(glShaderSource(*shader, 1, &source, NULL))
-    HP_GLCHECK(glCompileShader(*shader))
-    GLint compile_status;
-    HP_GLCHECK(glGetShaderiv(*shader, GL_COMPILE_STATUS, &compile_status))
-    if (!compile_status) {
-        luerror(log, "Failed to compile %s shader", shader_type_str(log, shader_type));
-        GLint log_length;
-        HP_GLCHECK(glGetShaderiv(*shader, GL_INFO_LOG_LENGTH, &log_length))
-        GLchar log_text[log_length];
-        HP_GLCHECK(glGetShaderInfoLog(*shader, log_length, NULL, log_text))
-        lulog_lines(log, lulog_level_debug, log_text);
-        status = HP_ERR_OPENGL;
-        goto exit;
-    }
-    luinfo(log, "Compiled %s shader", shader_type_str(log, shader_type));
-    LU_NO_CLEANUP
-}
-
-static int link_program(const lulog *log, const GLuint *shaders, size_t n_shaders) {
-    LU_STATUS
-    HP_GLCHECK(GLuint program = glCreateProgram())
-    for (size_t i = 0; i < n_shaders; ++i) {
-        HP_GLCHECK(glAttachShader(program, shaders[i]))
-    }
-    HP_GLCHECK(glLinkProgram(program))
-    GLint link_status;
-    HP_GLCHECK(glGetProgramiv(program, GL_LINK_STATUS, &link_status))
-    if (!link_status) {
-        luerror(log, "Failed to link program");
-        GLint log_length;
-        HP_GLCHECK(glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_length))
-        GLchar log_text[log_length];
-        HP_GLCHECK(glGetProgramInfoLog(program, log_length, NULL, log_text))
-        lulog_lines(log, lulog_level_debug, log_text);
-        status = HP_ERR_OPENGL;
-        goto exit;
-    }
-    for (size_t i = 0; i < n_shaders; ++i) {
-        HP_GLCHECK(glDetachShader(program, shaders[i]))
-    }
-    luinfo(log, "Linked program with %zu shaders", n_shaders);
-    LU_NO_CLEANUP
-}
-
-static int init(const lulog *log) {
+static int build_program(const lulog *log) {
     LU_STATUS
     GLuint shaders[2];
     LU_CHECK(compile_shader(log, GL_VERTEX_SHADER, vertex_shader, &shaders[0]))
@@ -111,34 +47,17 @@ static int init(const lulog *log) {
     LU_NO_CLEANUP
 }
 
-static int main_loop(const lulog *log) {
-
+static int with_glfw(const lulog *log) {
     LU_STATUS
     GLFWwindow *window = NULL;
-
-    // not clear to me to what extent these duplicate or conflict with glad
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, 1);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    LU_ASSERT(window = glfwCreateWindow(320, 320, "hexpilot", NULL, NULL),
-            HP_ERR_GLFW, log, "Could not create window")
-
-    glfwMakeContextCurrent(window);
-    LU_ASSERT(gladLoadGLLoader((GLADloadproc) glfwGetProcAddress),
-            HP_ERR_GLAD, log, "Could not load OpenGL via glad")
-    LU_ASSERT(GLVersion.major > 1, HP_ERR_OPENGL, log,
-            "Bad OpenGL version: %d.%d", GLVersion.major, GLVersion.minor)
-    luinfo(log, "OpenGL %s, GLSL %s",
-            glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
-
-    LU_CHECK(init(log))
+    LU_CHECK(create_glfw_context(log, &window))
+    LU_CHECK(load_opengl_functions(log))
+    LU_CHECK(build_program(log))
     while (!glfwWindowShouldClose(window)) {
         LU_CHECK(display(log))
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
 LU_CLEANUP
     glfwTerminate();
     LU_RETURN
@@ -156,7 +75,7 @@ int main(int argc, char** argv) {
     lulog_mkstderr(&LOG, lulog_level_debug);
     glfwSetErrorCallback(on_error);
     LU_ASSERT(glfwInit(), HP_ERR_GLFW, LOG, "Could not start GLFW")
-    LU_CHECK(main_loop(LOG))
+    LU_CHECK(with_glfw(LOG))
 LU_CLEANUP
     return status ? 1 : 0;
 }
