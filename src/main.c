4c6422e49a3784826cdc41d1cc4b0c8369c3275b
andrew cooke <andrew@acooke.org>
2016-06-28 14:26:01 -0400

    separate program creation

--- src/main.c
+++ src/main.c
@@ -22,7 +22,6 @@ static int init_opengl(lulog *log) {
     GL_CHECK(glDepthFunc(GL_LEQUAL))
     GL_CHECK(glDepthRange(0.0f, 1.0f))
     GL_CHECK(glEnable(GL_MULTISAMPLE))  // see also GLFW_SAMPLES in glfw.c
-//    GL_CHECK(glEnable(GL_DEPTH_CLAMP))
     LU_NO_CLEANUP
 }
 
